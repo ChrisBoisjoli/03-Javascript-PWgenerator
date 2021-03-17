1bd12f2916cf80c836921a31253dc422202157f2
ChrisBoisjoli <christopherboisjoli@gmail.com>
2021-03-17 18:34:19 -0500

    starting generate password function

--- script.js
+++ script.js
@@ -2,9 +2,20 @@
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
+
+function generatePassword(){
+    
+  var passwordLength = "";
+  while (passwordLength <8 || passwordLength > 128 || isNaN(passwordLength)){
+    prompt ("Select a password length between 8 and 128 characters");
+  }
+  
+};
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+
+
 
   passwordText.value = password;
 
